f715ddd75ede3ab3f70e3864b48afd84217b9f25
Bradenwg <bradenwgreene@gmail.com>
2023-12-01 16:52:44 -0500

    Updated Output

--- Battery_Cell_Config_Generator.py
+++ Battery_Cell_Config_Generator.py
@@ -12,6 +12,9 @@ def group_batteries(filename, num_cells_per_pack, num_packs):
 
     # Initialize empty packs
     packs = [[] for _ in range(num_packs)]
+    pack_ocvs = [[] for _ in range(num_packs)]
+    pack_capacity = [[] for _ in range(num_packs)]
+
     pack_sums = [0] * num_packs
 
     # Distribute batteries among packs to equalize capacities
@@ -22,22 +25,30 @@ def group_batteries(filename, num_cells_per_pack, num_packs):
         # If the pack isn't full, add the battery
         if len(packs[min_pack_index]) < num_cells_per_pack:
             packs[min_pack_index].append(row['Location'])
+            pack_ocvs[min_pack_index].append(row['Final OCV'])
+            pack_capacity[min_pack_index].append(row['Capacity (mAh)'])
             pack_sums[min_pack_index] += row['Capacity (mAh)']
 
-    return packs, pack_sums
+    return packs, pack_sums, pack_ocvs, pack_capacity
 
-def save_to_excel(packs, pack_sums, output_filename):
+def save_to_excel(packs, pack_sums, pack_ocvs, pack_capacity, output_filename):
     # Create a DataFrame from the packs
-    df_output = pd.DataFrame(packs).transpose()
-    df_output.columns = [f"Parallel Group {i+1}" for i in range(len(packs))]
-    df_output.loc['Total Capacity'] = pack_sums
-    # Save the DataFrame to an Excel file
+    df_output = pd.DataFrame()
+    for i, pack in enumerate(packs):
+        df_output[f'Pack {i+1} Location'] = pd.Series(pack)
+        df_output[f'Pack {i+1} OCV (V)'] = pd.Series(pack_ocvs[i])
+        df_output[f'Pack {i+1} Capacity (mAh)'] = pd.Series(pack_capacity[i])
+        
+        #df_output.loc['Total Capacity'] = pack_sums
+        # Save the DataFrame to an Excel file
     df_output.to_excel(output_filename, index=False)
 
 # Example
-filename = 'CellsWithin1STD.xlsx'
+filename = 'Battery Test Logger.xlsx'
 num_cells_per_pack = 14
 num_packs = 29
 
-packs, pack_sums = group_batteries(filename, num_cells_per_pack, num_packs)
-save_to_excel(packs, pack_sums, 'battery_packs_output.xlsx')
+packs, pack_sums, pack_ocvs, pack_capacity = group_batteries(filename, num_cells_per_pack, num_packs)
+save_to_excel(packs, pack_sums, pack_ocvs, pack_capacity,  'battery_packs_output.xlsx')
+
+
